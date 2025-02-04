448b3107b1a3cbcbffce9ea13e3a8130040722b2
YeraldoCode <yeramtza@gmail.com>
2025-02-04 01:09:22 -0600

    mneu inicial configuracion

--- src/pong.py
+++ src/pong.py
@@ -9,12 +9,9 @@ pantalla = pg.display.set_mode((ANCHO_PANTALLA, ALTO_PANTALLA))
 pg.display.set_caption("PongYera")
 
 
-
 #variables de inicalizacion 
 ejecutando = True
 mi_reloj = pg.time.Clock()
-puntos_para_ganar = 5
-
 
 #Paleta de colores 
 BLANCO = (255,255,255)
@@ -58,7 +55,7 @@ puntos_j2 = 0
 pg.font.init()
 calibri_bold_35 = pg.font.SysFont("calibri Bold", 35)
 calibri_bold_50 = pg.font.SysFont("calibri Bold", 50)
-
+calibri_bold_120 = pg.font.SysFont("calibri Bold", 120)
 
 
 #crear elementos del juego
@@ -81,6 +78,43 @@ def dibujar_pantalla():
     pantalla.blit(texto_puntos_j1, (130, 20))
     pantalla.blit(texto_puntos_j2, (620, 20))
 
+def mostrar_menu_inicial():
+    pantalla.fill(COLOR_FONDO)
+    titulo = calibri_bold_120.render('PONGYERA', True, ROJO)
+
+    pantalla.blit(titulo, (ANCHO_PANTALLA // 2 - titulo.get_width()//2, ALTO_PANTALLA // 2 - 200))
+
+    texto_inicio = calibri_bold_35.render('¿Ha Cuantos puntos se jugara la partida?' , True, ROJO)  
+    pantalla.blit(texto_inicio, (ANCHO_PANTALLA // 2 - texto_inicio.get_width()//2, ALTO_PANTALLA // 2 - 100))
+
+    opcion_5 = calibri_bold_35.render('5 (1)' , True, BLANCO)
+    pantalla.blit(opcion_5, (ANCHO_PANTALLA // 2 -  200 //2, ALTO_PANTALLA // 2 - 30))
+
+    opcion_7 = calibri_bold_35.render('7 (2)' , True, BLANCO)
+    pantalla.blit(opcion_7, (ANCHO_PANTALLA // 2 -  200 //2, ALTO_PANTALLA // 2 + 20))
+
+    opcion_10 = calibri_bold_35.render('10 (3)' , True, BLANCO)
+    pantalla.blit(opcion_10, (ANCHO_PANTALLA // 2 -  200 //2, ALTO_PANTALLA // 2 + 70))
+
+    pg.display.flip()
+
+
+    while True:
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                pg.quit()
+                quit()
+            if e.type == pg.KEYDOWN:
+                if e.key == pg.K_1:
+                    return 5
+                elif e.key == pg.K_2:
+                    return 7
+                elif e.key == pg.K_3:
+                    return 10
+        mi_reloj.tick(10)
+
+
+
 #verificar ganador 
 def verificar_ganador():
     if puntos_j1 >= puntos_para_ganar:
@@ -93,8 +127,30 @@ def mostrar_ganador():
     pantalla.fill(COLOR_FONDO)
     texto_ganador = calibri_bold_50.render('ha ganado ' + ganador + '!', True, BLANCO)
     pantalla.blit(texto_ganador, (ANCHO_PANTALLA//2 - texto_ganador.get_width()//2, ALTO_PANTALLA//2 -100))
+
+    texto_reinicio = calibri_bold_50.render('Quieres jugar de nuevo?', True, BLANCO)
+    pantalla.blit(texto_reinicio, (ANCHO_PANTALLA//2 - texto_reinicio.get_width()//2, ALTO_PANTALLA//2))
+
+    texto_si = calibri_bold_35.render('SI (s)', True, BLANCO)
+    pantalla.blit(texto_si, (ANCHO_PANTALLA//2 - 100 //2, ALTO_PANTALLA//2 + 50))
+
+    texto_no = calibri_bold_35.render('No (n)', True, BLANCO)
+    pantalla.blit(texto_no, (ANCHO_PANTALLA//2 + 50 //2, ALTO_PANTALLA//2 + 50))
+
+    
+
     pg.display.flip()
+
     while True:
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                pg.quit()
+                quit()
+            if e.type == pg.KEYDOWN:
+                if e.key == pg.K_s:
+                    return True
+                if e.key == pg.K_n:
+                    return False
         mi_reloj.tick(10)
 
 
@@ -112,81 +168,89 @@ def resetear_pelotas_y_pelotas():
 
 #Loop principal
 while ejecutando:
-    
-    #verificar cierre del juego 
-    for evento in pg.event.get():
-        if evento.type == pg.QUIT:
-            ejecutando = False
-    #evento al presionar tecla
-    teclas = pg.key.get_pressed()
 
-
-    #actualizar posisicones de elementos 
-    paleta_j1.y = j1_y
-    paleta_j2.y = j2_y
-    pelota.x = pelota_x
-    pelota.y = pelota_y
-
-
-
-    #redifinir coordenadas de las paletas
-    if teclas[pg.K_w] and j1_y > 0:
-        j1_y -= 5
-    elif teclas [pg.K_s] and j1_y + ALTO_PALETA < ALTO_PANTALLA:
-        j1_y += 5
-
-    
-    if teclas[pg.K_UP] and j2_y > 0:
-        j2_y -= 5
-    elif teclas [pg.K_DOWN] and j2_y + ALTO_PALETA < ALTO_PANTALLA:
-        j2_y += 5
-
-    #redifinir las coordenadas de la pelota 
-    pelota_x += pelota_diferencia_x
-    pelota_y += pelota_diferencia_y
-
-    #verificar colisiones en cada movimiento de la pelota
-    if pelota.colliderect(paleta_j1):
-        sonido_golpe_paleta.play()
-        pelota_diferencia_x = abs(pelota_diferencia_x)
-    elif pelota.colliderect(paleta_j2):
-        sonido_golpe_paleta.play()
-        pelota_diferencia_x = abs(pelota_diferencia_x)*-1
-    elif pelota_y <= 0:
-        sonido_golpe_pared.play()
-        pelota_diferencia_y = abs(pelota_diferencia_y)
-    elif pelota_y >= ALTO_PANTALLA:
-        sonido_golpe_pared.play()
-        pelota_diferencia_y = abs(pelota_diferencia_y)*-1
-    elif pelota_x <= 0 or pelota_x >= ANCHO_PANTALLA:
-        sonido_punto.play()
-        if pelota_x >= ANCHO_PANTALLA:
-            puntos_j1 += 1
-        elif pelota_x <= 0:
-            puntos_j2 += 1
+    #mostrar menu inicial 
+    puntos_para_ganar = mostrar_menu_inicial()
+    jugando = True
+    while jugando:
         
-        #verificar si hay ganador 
-        ganador = verificar_ganador()
-        if ganador:
-            mostrar_ganador()
+        #verificar cierre del juego 
+        for evento in pg.event.get():
+            if evento.type == pg.QUIT:
+                ejecutando = False
+                break
+        if not ejecutando:
+            break
+        #evento al presionar tecla
+        teclas = pg.key.get_pressed()
 
 
-        resetear_pelotas_y_pelotas()
-
-
-
-    dibujar_pantalla()
-
-
-    pg.display.flip()
-    mi_reloj.tick(60)
-
-
-
-
+        #actualizar posisicones de elementos 
+        paleta_j1.y = j1_y
+        paleta_j2.y = j2_y
+        pelota.x = pelota_x
+        pelota.y = pelota_y
 
 
 
+        #redifinir coordenadas de las paletas
+        if teclas[pg.K_w] and j1_y > 0:
+            j1_y -= 5
+        elif teclas [pg.K_s] and j1_y + ALTO_PALETA < ALTO_PANTALLA:
+            j1_y += 5
+
+        
+        if teclas[pg.K_UP] and j2_y > 0:
+            j2_y -= 5
+        elif teclas [pg.K_DOWN] and j2_y + ALTO_PALETA < ALTO_PANTALLA:
+            j2_y += 5
+
+        #redifinir las coordenadas de la pelota 
+        pelota_x += pelota_diferencia_x
+        pelota_y += pelota_diferencia_y
+
+        #verificar colisiones en cada movimiento de la pelota
+        if pelota.colliderect(paleta_j1):
+            sonido_golpe_paleta.play()
+            pelota_diferencia_x = abs(pelota_diferencia_x)
+        elif pelota.colliderect(paleta_j2):
+            sonido_golpe_paleta.play()
+            pelota_diferencia_x = abs(pelota_diferencia_x)*-1
+        elif pelota_y <= 0:
+            sonido_golpe_pared.play()
+            pelota_diferencia_y = abs(pelota_diferencia_y)
+        elif pelota_y >= ALTO_PANTALLA:
+            sonido_golpe_pared.play()
+            pelota_diferencia_y = abs(pelota_diferencia_y)*-1
+        elif pelota_x <= 0 or pelota_x >= ANCHO_PANTALLA:
+            sonido_punto.play()
+            if pelota_x >= ANCHO_PANTALLA:
+                puntos_j1 += 1
+            elif pelota_x <= 0:
+                puntos_j2 += 1
+            
+            #verificar si hay ganador 
+            ganador = verificar_ganador()
+            if ganador:
+                #preguntar si quieren jugar de nuevo 
+                #si ganador regresa con false 
+                if not mostrar_ganador():
+                    ejecutando = False
+                    jugando = False
+                    break
+                #si ganador vuelve con true 
+                else :
+                    #si decide jugar de nuevo reiniciar 
+                    puntos_j1 = 0 
+                    puntos_j2 = 0
+                    resetear_pelotas_y_pelotas()
+                    break
+            resetear_pelotas_y_pelotas()
+
+        dibujar_pantalla()
+
+        pg.display.flip()
+        mi_reloj.tick(60)
 
 
 pg.quit()
